658cbbbb197ddeccfb7ee5b0f63cdce62ea27420
KimMinheee <99alsgmlalsl@naver.com>
2023-04-07 23:42:14 +0900

    querydsl 설정 및 세팅

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,42 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em); //최근버전 JPAQueryFactory 권장
+		QHello qHello = new QHello("h");
+
+
+		//query와 관련된 것은 다 Q타입을 넣어야 한다.
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+
 	}
 
 }
